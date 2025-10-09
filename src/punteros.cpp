0d9c4cb56dfd3d7d90b0400a8867361fad2dace4
EMGC323 <a24310395@live.ceti.mx>
2025-10-09 01:58:42 +0000

    practica 2.1

--- src/punteros.cpp
+++ src/punteros.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 int main()
 {
     int *pNum = NULL;
@@ -10,10 +11,18 @@ int main()
     std::cout << "direccion: " << pNum << '\n';
     std::cout << "valor: " << pNum << '\n';
     std::cout << "direccion del puntero: " << &pNum << '\n';
-
-    std::cout << sizeof(pNum) << '\n';
-
     
+    std::cout << "Tipos de dato: " << '\n';
+
+    std::cout << "tamaño int: " << sizeof(int) << '\n';
+
+    std::cout << "Tamaño long: " << sizeof(long) << '\n';
+    std::cout << "Tamaño float: " << sizeof(float) << '\n';
+    std::cout << "Tamaño double: " << sizeof(double) << '\n';
+    std::cout << "Tamaño char: " << sizeof(char) << '\n';
+    std::cout << "Tamaño bool: " << sizeof(bool) << '\n';
+    std::cout << "Tamaño byte: " << sizeof(std::byte) << '\n';
+
 
     return 0;
 }
